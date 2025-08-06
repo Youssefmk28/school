a0f6723d1d7ae0ab84ef95d92f504129f55e34d2
Youssefmk28 <youssefmka991@gmail.com>
2025-08-06 13:58:49 -0700

    Update person.h

--- person.h
+++ person.h
@@ -2,23 +2,24 @@
 #include <iostream>
 #include <string>
 #include <vector>
+#include <limits>
 using namespace std;
 class person
 {
 private:
 	string Name = "",Gender = "",BirthDate = "";
-	string Age;
+	int Age = 0;
 public:
 	person();
-	person(string N, string G,string B, string A);
+	person(string N, string G,string B, int A);
 	void Set_Name(string N);
 	void Set_Gender(string G);
 	void Set_BirthDate(string B);
-	void Set_Age(string A);
+	void Set_Age(int A);
 	string Get_Name();
 	string Get_Gender();
 	string Get_BirthDate();
-	string Get_Age();
+	int Get_Age();
 	virtual void input();
 	virtual void display();
 };
